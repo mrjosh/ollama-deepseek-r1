842e1718a15c22da7a19219c5b618e10f1cc3710
Josh <josheghani.dev@gmail.com>
2025-11-03 22:14:28 +0330

    update

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,13 @@
 FROM ghcr.io/open-webui/open-webui:main as openwebui
 
 RUN apt update && apt install curl -y
-RUN curl -fsSL https://ollama.com/install.sh | sh
+RUN pip install vllm
 
-ENV MODELS="llama3 deepseek-r1:8b"
-ENV OLLAMA_BASE_URL="http://localhost:11434"
+ENV OLLAMA_BASE_URL="http://localhost:8000"
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
-EXPOSE 8080
+EXPOSE 8000
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,22 +6,12 @@ set -e
 # clear ready flag
 rm -f /tmp/ready
 
-ollama serve &
+vllm serve &
 
 # start ollama, wait for it to serve
-echo "Starting Ollama..."
-until curl -s http://localhost:11434 > /dev/null; do
+echo "Starting VLLM..."
+until curl -s http://localhost:8000 > /dev/null; do
   sleep 1
-done
-
-# pull and install models, or skip if they're present
-for MODEL in $MODELS; do
-  if ! ollama list | grep -q "$MODEL"; then
-    echo "⚡️ Pulling model: $MODEL"
-    ollama pull "$MODEL"
-  else
-    echo "⛳️ Model '$MODEL' already present."
-  fi
 done
 
 # set container as ready
